6a02f183d21b34cb92c6971314d5bb2af36ec65e
its-kaktos <arman.hmpr@gmail.com>
2024-12-16 09:15:53 +0330

    Reset log.go to previous state and move its content to cat_file.go

--- cmd/cat_file.go
+++ cmd/cat_file.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
+	"armanVersionControl/storage"
 	"armanVersionControl/storage/objectstore"
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 )
 
+var prettyPrint = false
 var catFileCmd = &cobra.Command{
-	Use:   "cat-file {hash}",
+	Use:   "cat-file {hash} ([-p | --pretty-print])",
 	Short: "Display content of object by its hash.",
 	Long: `This command will display the content of an object stored in object by its hash.
 
@@ -15,16 +18,55 @@ Arguments:
     hash		The required hash representing the object ID stored in object database.`,
 	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		content, err := objectstore.FetchByHash(args[0])
+		//content, err := objectstore.FetchByHash(args[0])
+		//if err != nil {
+		//	return err
+		//}
+		//
+		//fmt.Println(string(content))
+		//return nil
+		hash := args[0]
+		c, err := objectstore.FetchByHash(hash)
 		if err != nil {
 			return err
 		}
 
-		fmt.Println(string(content))
+		s, err := computeOriginalContent(c)
+		if err != nil {
+			return err
+		}
+
+		// the \033[1;32 part is the coloring. Read more at: https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+		fmt.Println("\033[1;32mHere is the content of provided hash:\033[0m")
+		fmt.Println(s)
+
 		return nil
 	},
 }
 
 func init() {
+	catFileCmd.Flags().BoolVarP(&prettyPrint, "pretty-print", "p", false, "Pretty print the content of hash object based on its type.")
 	RootCmd.AddCommand(catFileCmd)
+}
+
+func computeOriginalContent(b []byte) (string, error) {
+	if storage.IsBlobB(b) {
+		blob, err := storage.NewBlobFromB(b)
+		if err != nil {
+			if errors.Is(err, storage.ErrNotABlob) {
+				panic("expected a blob but the content is not a blob")
+			}
+
+			return "", err
+		}
+
+		return string(blob.Content), nil
+	}
+
+	// TODO do this for the Tree as well
+	if storage.IsTreeB(b) {
+
+	}
+
+	return "", errors.New("invalid content, content should either be a Blob or a Tree")
 }
--- cmd/log.go
+++ cmd/log.go
@@ -1,38 +1,25 @@
 package cmd
 
 import (
-	"armanVersionControl/storage"
 	"armanVersionControl/storage/objectstore"
-	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 )
 
-/*
-TODO
-- Remove printing all objects
-- Pass a hash with -p tag to tell print its content
-*/
 var logCmd = &cobra.Command{
-	Use:   "log hash",
-	Short: "Prints content of object associated with hash.",
-	Long:  "Prints content of object associated with hash, will do further processing to figure out the object type to print it in a readable format.",
-	Args:  cobra.ExactArgs(1),
+	Use:   "log",
+	Short: "Currently prints all objects stored in object database.",
+	Long:  "Currently prints all objects stored in object database.",
+	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		hash := args[0]
-		c, err := objectstore.FetchByHash(hash)
+		all, err := objectstore.FetchAllObjectNames()
 		if err != nil {
 			return err
 		}
 
-		s, err := computeOriginalContent(c)
-		if err != nil {
-			return err
+		for _, o := range all {
+			fmt.Println(o)
 		}
-
-		// the \033[1;32 part is the coloring. Read more at: https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
-		fmt.Println("\033[1;32mHere is the content of provided hash:\033[0m")
-		fmt.Println(s)
 
 		return nil
 	},
@@ -40,24 +27,4 @@ var logCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(logCmd)
-}
-
-func computeOriginalContent(b []byte) (string, error) {
-	if storage.IsBlobB(b) {
-		blob, err := storage.NewBlobFromB(b)
-		if err != nil {
-			if errors.Is(err, storage.ErrNotABlob) {
-				panic("expected a blob but the content is not a blob")
-			}
-
-			return "", err
-		}
-
-		return string(blob.Content), nil
-	}
-
-	// TODO do this for the Tree as well
-	//if storage.IsTreeB()
-
-	return "", errors.New("invalid content, content should either be a Blob or a Tree")
 }
